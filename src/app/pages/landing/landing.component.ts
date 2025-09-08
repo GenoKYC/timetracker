1c40ea728e152fd069304f19633304b63dd58c98
kyle <kylemarave5@gmail.com>
2025-09-09 00:20:51 +0800

    ui fixed

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -3,9 +3,9 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  standalone: true,            // ✅ Required if you're using `imports: []`
-  // imports: [RouterLink],
+  standalone: true, // ✅ Required if you're using `imports: []`
+  imports: [RouterLink],
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss'] // ✅ Corrected property name
+  styleUrls: ['./landing.component.scss'], // ✅ Corrected property name
 })
-export class LandingComponent { }
+export class LandingComponent {}
