ab34430ab6834a7a8f849c489edec04194fe11f4
kyle <kylemarave5@gmail.com>
2025-09-10 20:57:34 +0800

    maints

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { importProvidersFrom } from '@angular/core';
-
-import { HttpClientModule } from '@angular/common/http';
-
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import Swal from 'sweetalert2';
-import { LoginComponent } from './app/pages/auth/login/login.component';
 
-bootstrapApplication(LoginComponent, {
-  providers: [
-    importProvidersFrom(HttpClientModule), // 👈 add here if you don’t want to add to component
-  ],
-});
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
