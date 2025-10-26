a159593064e53083624df59a7a41f537f438ca7f
kyle <kylemarave5@gmail.com>
2025-10-26 18:02:08 +0800

    update dashboard- dtr front end

--- src/app/pages/main/employee/employee.component.ts
+++ src/app/pages/main/employee/employee.component.ts
@@ -1,11 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+interface Stat {
+  label: string;
+  value: number;
+  color: string;
+}
+
+interface TimeRecord {
+  employee: string;
+  contact: string;
+  position: string;
+  roles: string;
+  status: string;
+  statusColor: string;
+  hire: string;
+}
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee.component.html',
-  styleUrl: './employee.component.scss'
+  styleUrl: './employee.component.scss',
 })
 export class EmployeeComponent {
+  stats: Stat[] = [
+    { label: 'Total Employee', value: 14, color: '#000000' },
+    { label: 'Present Today', value: 6, color: '#16A34A' },
+    { label: 'On Leave', value: 2, color: '#3B82F6' },
+    { label: 'Absent', value: 1, color: '#DC2626' },
+  ];
 
+  tableHeaders = ['Employee', 'Contact', 'Position', 'Roles', 'Status', 'Hire'];
+
+  timeRecords: TimeRecord[] = [
+    {
+      employee: 'Kyle Marave',
+      contact: 'Photographer',
+      position: 'Secret',
+      roles: 'Present',
+      statusColor: '#16a34a',
+      status: 'Present',
+      hire: 'Jan 30, 2006',
+    },
+  ];
 }
--- src/app/pages/main/pos-and-roles/pos-and-roles.component.ts
+++ src/app/pages/main/pos-and-roles/pos-and-roles.component.ts
@@ -1,9 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+interface Stat {
+  label: string;
+  value: number;
+  color: string;
+}
+
+interface TimeRecord {
+  position: string;
+  description: string;
+  employees: string;
+}
 @Component({
   selector: 'app-pos-and-roles',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pos-and-roles.component.html',
   styleUrl: './pos-and-roles.component.scss',
 })
-export class PosAndRolesComponent {}
+export class PosAndRolesComponent {
+  // stats: Stat[] = [
+  //   { label: 'Total Employee', value: 14, color: '#000000' },
+  //   { label: 'Present Today', value: 6, color: '#16A34A' },
+  //   { label: 'On Leave', value: 2, color: '#3B82F6' },
+  //   { label: 'Absent', value: 1, color: '#DC2626' },
+  // ];
+
+  tableHeaders = ['Position', 'Description', 'Employees'];
+
+  timeRecords: TimeRecord[] = [
+    {
+      position: 'Contractual',
+      description: 'Full-time contractual ',
+      employees: '12',
+    },
+    {
+      position: 'Part-Timer',
+      description: 'Works < 20 hrs/week ',
+      employees: '4',
+    },
+    {
+      position: 'Project-Based',
+      description: 'Per project contract ',
+      employees: '8',
+    },
+  ];
+}
